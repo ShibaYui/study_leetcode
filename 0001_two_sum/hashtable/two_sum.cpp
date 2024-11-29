4f8c63011825b2658ed720ae00832a12091de0a3
ShibaYui <ai.brs.miku@gmail.com>
2024-11-30 02:03:52 +0800

    Initial commit

---
+++ 0001_two_sum/hashtable/two_sum.cpp
@@ -0,0 +1,93 @@
+/*
+	Filename: two_sum.cpp
+	Author: yu1
+	Date: 2024-11-29 23:32:50 +0800
+
+	Description:
+		Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+		You may assume that each input would have exactly one solution, and you may not use the same element twice.
+		You can return the answer in any order.
+
+	Example 1:
+		Input: nums = [2,7,11,15], target = 9
+		Output: [0,1]
+		Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+		
+	Example 2:
+		Input: nums = [3,2,4], target = 6
+		Output: [1,2]
+
+	Example 3:
+		Input: nums = [3,3], target = 6
+		Output: [0,1]
+*/
+#include<iostream>
+#include<vector>
+#include<fstream>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+	//使用巢狀loop
+	for (int i=0; i<nums.size(); ++i) {
+		for (int j=i+1; j<nums.size(); ++j) {
+			if (nums[i]+nums[j]==target) {
+				return {i, j};
+			}
+		}
+	}
+	return {};	//如果無解
+}
+
+
+int main() {
+	//開啟檔案
+	ifstream inputFile("input");
+
+	//如"input"不存在
+	if (!inputFile) {
+		cerr << "testcase does NOT exist" <<endl;
+		return 1;
+	}
+	
+	/*
+	使用while-loop將文件內容存入array "nums"
+	input格式：
+		a1 a2 a3 .. aN
+		target
+	
+	*/
+	//初始化array "nums"
+	vector<int> nums;
+	int target;
+	int number;
+	while (inputFile >> number) {
+		nums.push_back(number);	
+	}
+	
+	//關閉檔案
+	inputFile.close();
+
+	//檢查測資
+	//至少要有兩行數據
+	if (nums.size() < 2) {
+		cerr << "TestCase is ERROR" << endl;
+		return 1;
+	}
+	
+	//指定最後一行為input_target
+	target=nums.back();
+	nums.pop_back();
+
+	//輸出結果
+	//如無解答則另外輸出訊息
+	//if-loop
+	vector<int> result=twoSum(nums, target);
+	if (!result.empty()) {
+		cout << result[0] << " " << result[1] << endl;
+	}
+	else {
+		cout << "No Result" << endl;
+	}
+
+	return 0;
+}
---
+++ 0053_maximun_subarray/maximun_subarray.cpp
@@ -0,0 +1,86 @@
+/*
+	Filename: maximun_subarray.cpp
+	Author: yu1
+	Date: 2024-11-29 21:56:00 +0800
+
+	Description:
+		Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 	Example 1:
+		Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+		Output: 6
+		Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+	Example 2:
+		Input: nums = [1]
+		Output: 1
+		Explanation: The subarray [1] has the largest sum 1.
+
+	Example 3:
+		Input: nums = [5,4,-1,7,8]
+		Output: 23
+		Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+	
+	Constraints:
+		1 <= nums.length <= 105
+		-104 <= nums[i] <= 104
+
+	Follow up: 
+		If you have figured out the O(n) solution, 
+		try coding another solution using the divide and conquer approach, which is more subtle. 
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<fstream>	//讀取外部檔案
+using namespace std;
+
+//將testcase存入陣列，以指標指出（指標型別：int）
+int maxSubArray(vector<int>& nums) {
+	//以DP計算
+	//規劃模式：兩項之和（上次結果&下一項），與上次的結果比較，如較大則更新結果
+	//從nums[0]開始代入
+	//初始化總和
+	//初始化紀錄_總和
+	int max_sum=nums[0];
+	int noted_sum=nums[0];
+	for (int i=1; i<nums.size(); ++i) {
+		//計算兩項之和，並紀錄於noted_sum
+		//得到較大的值，並紀錄於max_sum
+		noted_sum=max(nums[i], noted_sum+nums[i]);
+		//用max()確保輸出必為最大值
+		max_sum=max(max_sum, noted_sum);
+	}
+	return {max_sum};
+}
+
+int main() {
+	//開啟檔案
+	ifstream inputFile("input");
+
+	//如"input"不存在
+	if (!inputFile) {
+		cerr << "testcase does NOT exist" <<endl;
+		return 1;
+	}
+	
+	//將文件內容存入array
+	//初始化array
+	vector<int> nums;
+	int iNumber;
+	while (inputFile >> iNumber) {
+		nums.push_back(iNumber);
+	}
+	
+	//關閉檔案
+	inputFile.close();
+
+	//如檔案為空，回傳錯誤
+	if(nums.empty()) {
+		cerr << "testcase is EMPTY." << endl;
+		return 1;
+	}
+	cout << maxSubArray(nums) << endl;
+
+	return 0;
+}
